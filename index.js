634d1227602e7e0fc26bbae47ae2816972a743c8
logan-gosch <logan.gosch.temp@gmail.com>
2024-10-22 20:45:40 -0500

    header updates

--- index.js
+++ index.js
@@ -27,9 +27,12 @@ function appendData(data) {
 
     div.classList.add("col-lg-4");
     div.innerHTML = `
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>${data.name}</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/><image src="${data.photo}" /></svg>
-              <h2 class="fw-normal">${data.name}</h2>
-              <p>${data.description}</p>
+        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <title>${data.name}</title>
+        <rect width="100%" height="100%" fill="var(--bs-secondary-color)"/>
+        </svg>
+        <h2 class="fw-normal">${data.name}</h2>
+        <p>${data.description}</p>
     `;
 
     mainContainer.appendChild(div);
